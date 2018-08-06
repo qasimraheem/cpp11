726e56253535d0d55346377aec3d26a145b6e5b6
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-08-06 14:54:43 +0500

    Basic C++

---
+++ 1. basic/11.Const Assignment/cmake-build-debug/CMakeFiles/11_Const_Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11_Const_Assignment.dir/main.cpp.o"
+  "11_Const_Assignment.pdb"
+  "11_Const_Assignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_Const_Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1. basic/11.Const Assignment/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    int x = 5 ;
+    const int MAX = 12 ;
+    int &ref_x1 = x ;
+    const int &ref_x2 = x ;
+
+//Try to modify the pointer (e.g. ptr1) and the pointee value (*ptr1)
+    const int *ptr1 = &x ;
+    int *const ptr2 = &x ;
+    const int * const ptr3 = &x ;
+
+//Find which declarations are valid. If invalid, correct the declaration
+    const int *ptr3 = &MAX ;
+    int *ptr4 = &MAX ;
+
+    const int &r1 = ref_x1 ;
+    int &r2 = ref_x2 ;
+
+    int *&p_ref1 = ptr1 ;
+    const int*&p_ref2 = ptr2 ;
+    return 0;
+}
