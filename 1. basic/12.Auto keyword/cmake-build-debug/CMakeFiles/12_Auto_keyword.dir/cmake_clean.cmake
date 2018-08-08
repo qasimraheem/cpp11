91c9efe161d138545d8600332717063f21402d69
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-08-08 10:37:01 +0500

    Basic C++

---
+++ 1. basic/12.Auto keyword/cmake-build-debug/CMakeFiles/12_Auto_keyword.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12_Auto_keyword.dir/main.cpp.o"
+  "12_Auto_keyword.pdb"
+  "12_Auto_keyword"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_Auto_keyword.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
