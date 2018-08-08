c985c7fe70f4290b2ea16f2e517ba59455c618d2
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-08-08 05:41:19 +0500

    Basic C++

--- 1. basic/11.Const Assignment/main.cpp
+++ 1. basic/11.Const Assignment/main.cpp
@@ -12,13 +12,12 @@ int main() {
     const int * const ptr3 = &x ;
 
 //Find which declarations are valid. If invalid, correct the declaration
-    //const int *ptr3 = &MAX ;
-//    int *ptr4 = &MAX ;
+    const int *ptr4 = &MAX ;
 
-//    const int &r1 = ref_x1 ;
-//    int &r2 = ref_x2 ;
+    const int &r1 = ref_x1 ;
+    const int &r2 = ref_x2 ;
 
-//    int *&p_ref1 = ptr1 ;
-//    const int*&p_ref2 = ptr2 ;
+    const int *&p_ref1 = ptr1 ;
+    const int *const &p_ref2 = ptr2;
     return 0;
 }
