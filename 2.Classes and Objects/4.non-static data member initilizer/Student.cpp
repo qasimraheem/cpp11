e76824b69eeb3aa7173fd78e9206a0a2cb4c8101
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-08-13 08:49:00 +0500

    C++11

---
+++ 2.Classes and Objects/4.non-static data member initilizer/Student.cpp
@@ -0,0 +1,36 @@
+//
+// Created by qasim on 8/12/18.
+//
+
+#include "Student.h"//linking Student.h
+
+Student::Student() {//constructor
+    cout<<"\nStudent";
+}
+Student::Student(string name) {//constructor by arguments
+    this->name=name;
+}
+Student::~Student() {//destructor
+    cout<<"\n~Student";
+}
+//defining functions of Student class
+void Student::setname(string name){
+    this->name=name;
+}
+void Student::setage(int age){
+    this->age=age;
+}
+void Student::setpoints(float points){
+    this->points=points;
+}
+string Student::getname(){
+    return name;
+}
+int Student::getage(){
+    return age;
+}
+float Student::getpoints(){
+    return points;
+}
+
+
