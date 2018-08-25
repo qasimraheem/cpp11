0fd16f48b59e9854fbaa6538a90fd59ba53d1e5f
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-08-25 14:44:21 +0500

    Object Oriented Programming

---
+++ 2.Classes and Objects/12.L-values, R-values/cmake-build-debug/CMakeFiles/12_L_values__R_values.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/12_L_values__R_values.dir/main.cpp.o"
+  "12_L_values__R_values.pdb"
+  "12_L_values__R_values"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_L_values__R_values.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
