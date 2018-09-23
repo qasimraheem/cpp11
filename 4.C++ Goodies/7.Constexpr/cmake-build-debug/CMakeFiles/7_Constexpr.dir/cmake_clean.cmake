413784c18827a1a16c81bf8abd1fbcab4a277b4e
qasim raheem mac <qasimmehmood13936@gmail.com>
2018-09-23 15:12:01 +0500

    Constexpr C++11

---
+++ 4.C++ Goodies/7.Constexpr/cmake-build-debug/CMakeFiles/7_Constexpr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/7_Constexpr.dir/main.cpp.o"
+  "7_Constexpr.pdb"
+  "7_Constexpr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/7_Constexpr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
